4a337e4b7dc9c166f5adad71b7e5b5c6b90c1f02
Wayne Hsiao <chronicqazxc@gmail.com>
2020-10-04 17:45:03 +0800

    Initial Commit

---
+++ GithubActionDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubActionDemo
+//
+//  Created by Wayne Hsiao on 2020/10/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubActionDemo/GithubActionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionDemoApp.swift
+//  GithubActionDemo
+//
+//  Created by Wayne Hsiao on 2020/10/4.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
